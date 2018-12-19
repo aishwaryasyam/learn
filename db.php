ff370cc99b6cd1c7426562a48c7dbc5ae9842441
aishwaryasyam <aishwaryarnair5@gmail.com>
2018-12-19 22:55:48 +0530

    Added database intitial configuration

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="root";
+$dbname="php_learn";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn)
+{
+die("Connection failed".mysqli_connect_error());
+}
+?>
